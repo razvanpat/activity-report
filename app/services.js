4d89fdbd37f6c35a3beae10cd2f7c43ede284ad4
razvan <razvan.pat@gmail.com>
2015-01-18 09:06:46 +0200

    fixed services declaration

--- app/services.js
+++ app/services.js
@@ -2,3 +2,5 @@ var app = require('angular').module('ActivityReport');
 
 app.factory('ReportsService', require('./services/ReportsService'));
 app.factory('SettingsService', require('./services/SettingsService'));
+app.factory('WindowService', require('./services/WindowService'));
+app.factory('Utils', require('./services/Utils'));
